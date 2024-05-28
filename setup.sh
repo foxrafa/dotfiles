ed62c1deefa4ab6f3662486ff883d4d7e7bd7153
foxrafa <20726634+foxrafa@users.noreply.github.com>
2024-05-28 14:35:18 -0300

    a

--- setup.sh
+++ setup.sh
@@ -27,4 +27,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	curl -LO https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
 	sudo rm -rf /opt/nvim
 	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
