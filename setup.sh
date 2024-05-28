53da8ed53e8d547febcd034424a1c2f1cac96ecc
foxrafa <20726634+foxrafa@users.noreply.github.com>
2024-05-28 14:31:06 -0300

    a

--- setup.sh
+++ setup.sh
@@ -3,22 +3,28 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "This is a linux machine."
 
+	sudo passwd root
+	sudo passwd fox
+	sudo apt-get update
+
 	if ! command -v zsh &>/dev/null; then
 		echo "Zsh is not installed. Installing..."
-		sudo passwd root
-		sudo passwd fox
-		sudo apt-get update
 		sudo apt-get install zsh -y
 		chsh -s "$(which zsh)"
 	else
 		echo "Zsh is already installed."
 	fi
 
+	sudo apt-get install git build-essential
+
 	ln -s ~/dotfiles/.zprofile ~/.zprofile
 	ln -s ~/dotfiles/.zshrc ~/.zshrc
 	ln -s ~/dotfiles/.zsh ~/.zsh
+
 	mkdir ~/.config
 	ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 
-	sudo apt update && sudo apt install python3 nodejs git htop build-essential
+	curl -LO https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
 fi
