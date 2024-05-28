02253665eea4dbec15519e795006a00e33653d6a
foxrafa <20726634+foxrafa@users.noreply.github.com>
2024-05-28 16:11:55 -0300

    a

--- download.py
+++ download.py
@@ -3,3 +3,7 @@ import gdown
 url = "https://drive.google.com/uc?id=14nbJUUg80Jkg_SaWma7kE4elpkmzEEVT"
 output = "NVIDIA-Linux-x86_64-550.54.15.run"
 gdown.download(url, output)
+
+url = "https://drive.google.com/uc?id=11id1RRBsKc8epbdsoTxYOzdfecSpzJ-z"
+output = "cudnn-linux-x86_64-8.9.2.26_cuda12-archive.tar.xz"
+gdown.download(url, output)
